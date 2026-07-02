d9cdfa055ac3ac1cd5b9aa31bb4ea38808820bff
zylau <zylau@live.fr>
2026-07-02 15:13:39 +0200

    Gazebo Harmonic migration and adding sdf file for simulation with lidar and camera

--- ROS/osr_gazebo/launch/empty_world.launch.py
+++ ROS/osr_gazebo/launch/empty_world.launch.py
@@ -4,9 +4,10 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, DeclareLaunchArgument
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
@@ -14,40 +15,73 @@ import xacro
 
 
 def generate_launch_description():
+
+    package_name = 'osr_gazebo'
+
+    default_world = os.path.join(get_package_share_directory(package_name), 'worlds', 'island.sdf')
+
+    world = LaunchConfiguration('world')
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+    )
+
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+                    get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+                    launch_arguments={
+                        'gz_args': ['-r -v4 ', world],
+                        'on_exit_shutdown': 'true'
+                    }.items()
              )
 
-    osr_urdf_path = os.path.join(
-        get_package_share_directory('osr_gazebo'))
-
-    xacro_file = os.path.join(osr_urdf_path,
-                              'urdf',
-                              'osr.urdf.xacro')
-
-    doc = xacro.parse(open(xacro_file))
-    xacro.process_doc(doc)
-    params = {'robot_description': doc.toxml()}
+    xacro_file = os.path.join(get_package_share_directory(package_name), 'urdf', 'osr.urdf.xacro')
+    robot_description_urdf = xacro.process_file(xacro_file).toxml()
 
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[{
+            'robot_description': robot_description_urdf,
+            'use_sim_time': True
+        }]
     )
 
-    controller_spawn = Node(
-        package='osr_gazebo',
+    node_controller_spawn = Node(
+        package=package_name,
         executable='osr_controller',
         output='screen'
     )
     
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'rover'],
-                        output='screen')
+    node_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', 'robot_description',
+            '-name', 'rover',
+            '-z', '0.8',            # Pour surelever si il se bug dans le sol
+        ],
+        output='screen'
+    )
 
+    bridge_params = os.path.join(get_package_share_directory(package_name), 'config', 'gz_bridge.yaml')
+    node_ros_gz_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        output='screen',
+        parameters=[{
+            'config_file': bridge_params
+        }]
+    )
+
+    node_ros_gz_image = Node(
+        package='ros_gz_image',
+        executable='image_bridge',
+        output='screen',
+        arguments=['/camera/image_raw']
+    )
 
     # joint_state_controller
     load_joint_state_controller = ExecuteProcess(
@@ -66,12 +100,22 @@ def generate_launch_description():
         cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'servo_controller'],
         output='screen'
     )
+
+    # Launch RViz
+    rviz_config_file = os.path.join(get_package_share_directory(package_name), 'rviz/rviz_settings2.rviz')
+    node_rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_file]
+    )
     
     return LaunchDescription([
-    	controller_spawn,
+    	node_controller_spawn,
         RegisterEventHandler(
             event_handler=OnProcessExit(
-                target_action=spawn_entity,
+                target_action=node_spawn_entity,
                 on_exit=[
                     load_joint_state_controller,
                     rover_wheel_controller,
@@ -80,7 +124,11 @@ def generate_launch_description():
             )
         ),
    
+        world_arg,
         gazebo,
         node_robot_state_publisher,
-        spawn_entity,
+        node_spawn_entity,
+        node_ros_gz_bridge,
+        node_rviz,
+        node_ros_gz_image,
     ])
