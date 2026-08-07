e7d57fd4e6219fa103829e818e41fbfc24bb4e65
zylau <zylau@live.fr>
2026-08-07 14:43:03 +0200

    change naV2 config and osr_launch structure

--- ROS/osr_bringup/launch/osr_launch.py
+++ ROS/osr_bringup/launch/osr_launch.py
@@ -1,7 +1,12 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    RegisterEventHandler,
+)
+from launch.event_handlers import OnProcessExit
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
@@ -11,139 +16,162 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
 
     roboclaw_params = os.path.join(
-        get_package_share_directory('osr_bringup'),
-        'config',
-        'roboclaw_params.yaml'
+        get_package_share_directory("osr_bringup"), "config", "roboclaw_params.yaml"
     )
     osr_params = os.path.join(
-        get_package_share_directory('osr_bringup'),
-        'config',
-        'osr_params.yaml'
+        get_package_share_directory("osr_bringup"), "config", "osr_params.yaml"
     )
 
-    ld = LaunchDescription()
-    
-    ld.add_action(
-        Node(
-            package='osr_control',
-            executable='roboclaw_wrapper',
-            name='roboclaw_wrapper',
-            output='screen',
-            emulate_tty=True,
-            respawn=True,
-            parameters=[roboclaw_params]
-        )
+    node_roboclaw = Node(
+        package="osr_control",
+        executable="roboclaw_wrapper",
+        name="roboclaw_wrapper",
+        output="screen",
+        emulate_tty=True,
+        respawn=True,
+        parameters=[roboclaw_params],
     )
-    ld.add_action(
-        Node(
-            package='osr_control',
-            executable='servo_control',
-            name='servo_wrapper',
-            output='screen',
-            emulate_tty=True,
-            respawn=True,
-            parameters=[{'centered_pulse_widths': [147, 165, 160, 152]}]  # pulse width where the corner motors are in their default position, see rover_bringup.md.
-            # indices du tableau :
-            # 0 = back_right
-            # 1 = front_right
-            # 2 = front_left
-            # 3 = back_left
-        )
+
+    node_servo = Node(
+        package="osr_control",
+        executable="servo_control",
+        name="servo_wrapper",
+        output="screen",
+        emulate_tty=True,
+        respawn=True,
+        parameters=[
+            {"centered_pulse_widths": [147, 165, 160, 152]}
+        ],  # pulse width where the corner motors are in their default position, see rover_bringup.md.
+        # indices du tableau :
+        # 0 = back_right
+        # 1 = front_right
+        # 2 = front_left
+        # 3 = back_left
     )
-    ld.add_action(
-        DeclareLaunchArgument('enable_odometry', default_value='false')
+
+    arg_enable_odom = DeclareLaunchArgument("enable_odometry", default_value="false")
+    arg_publish_tf = DeclareLaunchArgument("publish_transform", default_value="true")
+    node_rover = Node(
+        package="osr_control",
+        executable="rover",
+        name="rover",
+        output="screen",
+        emulate_tty=True,
+        respawn=True,
+        parameters=[
+            osr_params,
+            {
+                "enable_odometry": LaunchConfiguration("enable_odometry"),
+                "publish_transform": LaunchConfiguration("publish_transform"),
+            },
+        ],
     )
-    ld.add_action(
-        DeclareLaunchArgument('publish_transform', default_value='true')
+
+    node_teleop = Node(
+        package="teleop_twist_joy",
+        executable="teleop_node",
+        name="teleop_twist_joy",
+        output="screen",
+        emulate_tty=True,
+        respawn=True,
+        parameters=[
+            {
+                "scale_linear.x": 1.3
+            },  # scale to apply to drive speed, in m/s: drive_motor_rpm * 2pi / 60 * wheel radius * slowdown_factor
+            {"axis_linear.x": 1},  # ! xbox=1 !
+            {
+                "axis_angular.yaw": 0
+            },  # ! xbox=0 ! which joystick axis to use for driving
+            {"axis_angular.pitch": 2},  # ! xbox=2 ! axis to use for in-place rotation
+            {
+                "scale_angular.yaw": 2.88
+            },  # scale to apply to angular speed, in rad/s: scale_linear / min_radius(=0.45m)
+            {
+                "scale_angular.pitch": 2.88
+            },  # scale to apply to angular speed, in rad/s: scale_linear / min_radius(=0.45m)
+            {
+                "scale_angular_turbo.yaw": 3.88
+            },  # scale to apply to angular speed, in rad/s: scale_linear_turbo / min_radius
+            {"scale_linear_turbo.x": 1.75},  # scale to apply to linear speed, in m/s
+            {"enable_button": 7},  # ! xbox=7 ! which button to press to enable movement
+            {"enable_turbo_button": 6},  # ! xbox=6 ! -1 to disable turbo
+            {"require_enable_button": True},
+        ],
+        remappings=[("/cmd_vel", "/cmd_vel_intuitive")],
     )
-    ld.add_action(
-        Node(
-            package='osr_control',
-            executable='rover',
-            name='rover',
-            output='screen',
-            emulate_tty=True,
-            respawn=True,
-            parameters=[osr_params,
-                        {'enable_odometry': LaunchConfiguration('enable_odometry'),
-                         'publish_transform': LaunchConfiguration('publish_transform')}]
-        )
+
+    node_joy = Node(
+        package="joy",
+        executable="joy_node",
+        name="joy",
+        output="screen",
+        emulate_tty=True,
+        respawn=True,
+        parameters=[
+            {"deadzone": 0.1},
+            {"autorepeat_rate": 5.0},
+            {
+                "device_id": 0
+            },  # ! default = 0 ! This might be different on your computer. Run `ls -l /dev/input/event*`. If you have event1, put 1.
+        ],
     )
-    ld.add_action(
-        Node(
-            package='teleop_twist_joy',
-            executable='teleop_node',
-            name='teleop_twist_joy',
-            output='screen',
-            emulate_tty=True,
-            respawn=True,
-            parameters=[
-                {"scale_linear.x": 1.3}, # scale to apply to drive speed, in m/s: drive_motor_rpm * 2pi / 60 * wheel radius * slowdown_factor
-                {"axis_linear.x": 1}, # ! xbox=1 !
-                {"axis_angular.yaw": 0},  # ! xbox=0 ! which joystick axis to use for driving
-                {"axis_angular.pitch": 2},  # ! xbox=2 ! axis to use for in-place rotation
-                {"scale_angular.yaw": 2.88},  # scale to apply to angular speed, in rad/s: scale_linear / min_radius(=0.45m)
-                {"scale_angular.pitch": 2.88},  # scale to apply to angular speed, in rad/s: scale_linear / min_radius(=0.45m)
-                {"scale_angular_turbo.yaw": 3.88},  # scale to apply to angular speed, in rad/s: scale_linear_turbo / min_radius
-                {"scale_linear_turbo.x": 1.75},  # scale to apply to linear speed, in m/s
-                {"enable_button": 7},  # ! xbox=7 ! which button to press to enable movement
-                {"enable_turbo_button": 6},  # ! xbox=6 ! -1 to disable turbo
-                {"require_enable_button": True}
-            ],
-            remappings=[
-                ('/cmd_vel', '/cmd_vel_intuitive')
-            ]
-        )
+
+    node_ina = Node(
+        package="osr_control",
+        executable="ina260",
+        name="ina260_node",
+        output="screen",
+        emulate_tty=True,
+        parameters=[
+            {"publish_rate": 1.0},
+            {"sensor_address": "0x45"},
+        ],
     )
-    ld.add_action(
-        Node(
-            package='joy',
-            executable='joy_node',
-            name='joy',
-            output='screen',
-            emulate_tty=True,
-            respawn=True,
-            parameters=[
-                {"deadzone": 0.1},
-                {"autorepeat_rate": 5.0},
-                {"device_id": 0},  # ! default = 0 ! This might be different on your computer. Run `ls -l /dev/input/event*`. If you have event1, put 1.
-            ]        
-        )
-    )
-    ld.add_action(
-        Node(
-            package='osr_control',
-            executable='ina260',
-            name='ina260_node',
-            output='screen',
-            emulate_tty=True,
-            parameters=[
-                {"publish_rate": 1.0},
-                {"sensor_address": "0x45"},
-            ]        
-        )
-    )
-    # ld.add_action(
-    #     Node(
-    #         package='osr_control',
-    #         executable='joy_extras',
-    #         output='screen',
-    #         emulate_tty=True,
-    #         parameters=[
-    #             {"duty_button_index": 1}  # which button toggles duty mode on/off
-    #         ]
-    #     )
+
+    # node_joy_extras = Node(
+    #     package="osr_control",
+    #     executable="joy_extras",
+    #     output="screen",
+    #     emulate_tty=True,
+    #     parameters=[{"duty_button_index": 1}],  # which button toggles duty mode on/off
     # )
 
-    lidar = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('rplidar_ros'), 'launch'), '/rplidar_a1_launch.py']),
+    launch_lidar = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("rplidar_ros"), "launch"),
+                "/rplidar_a1_launch.py",
+            ]
+        ),
     )
-    ld.add_action(lidar)
-    rf2o = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('rf2o_laser_odometry'), 'launch'), '/rf2o_laser_odometry.launch.py']),
-    )
-    ld.add_action(rf2o)
 
-    return ld
+    launch_rf2o = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("rf2o_laser_odometry"), "launch"
+                ),
+                "/rf2o_laser_odometry.launch.py",
+            ]
+        ),
+    )
+
+    return LaunchDescription(
+        [
+            arg_enable_odom,
+            arg_publish_tf,
+            node_roboclaw,
+            node_servo,
+            node_rover,
+            node_teleop,
+            node_joy,
+            node_ina,
+            launch_lidar,
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=launch_lidar,
+                    on_exit=[launch_rf2o],
+                )
+            ),
+        ]
+    )
